2a78b5e9c2004e8c982c1a53112992ac47f1d723
Liquid Solid <liquid.solid@protonmail.com>
2020-08-16 14:53:09 +0300

    Remove unneccesary .rounded

--- src/components/ExampleNestedMousedownBlock.vue
+++ src/components/ExampleNestedMousedownBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="border border-green-300 rounded p-4 rounded-full h-full relative cursor-pointer"
+    class="border border-green-300 p-4 rounded-full h-full relative cursor-pointer"
     @mousedown.stop
     v-ripple="{
       class: 'bg-green-500 opacity-25',
