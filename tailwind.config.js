172df748abfe766ef2bcd2b1f5d5b51b52e6b7be
Liquid Solid <liquid.solid@protonmail.com>
2020-08-18 21:23:30 +0300

    Rubik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
       },
       transitionTimingFunction: {
         'md-standard': 'cubic-bezier(0.4, 0, 0.2, 1)'
-      }
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif']
+      },
     }
   },
   variants: {
